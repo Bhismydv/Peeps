8e7e4ba3a4b7b905349dc44746fee61324de5876
Bhism Pratap Yadav <bhismydv2912@gmail.com>
2020-04-28 14:16:04 +0530

    changes

--- Peeps/app/src/main/java/com/example/peeps/RegisterActivity.java
+++ Peeps/app/src/main/java/com/example/peeps/RegisterActivity.java
@@ -1,43 +1,14 @@
 package com.example.peeps;
 
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
-
-
-import com.google.firebase.auth.FirebaseAuth;
-
-
-
 public class RegisterActivity extends AppCompatActivity {
-
-    EditText full_name, email, password;
-    Button sign_up;
-    TextView txt_log_in;
-
-    FirebaseAuth auth;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        full_name =  findViewById(R.id.full_name);
-        email =  findViewById(R.id.email);
-        password =  findViewById(R.id.password);
-        sign_up=  findViewById(R.id.sign_up);
-        txt_log_in = findViewById(R.id.txt_login);
-
-        auth = FirebaseAuth.getInstance();
-
-
-
+        setContentView(R.layout.activity_register);
     }
 }
--- Peeps/app/src/main/java/com/example/peeps/SplashActivity.java
+++ Peeps/app/src/main/java/com/example/peeps/SplashActivity.java
@@ -1,27 +1,49 @@
 package com.example.peeps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
+import android.view.View;
+import android.view.ViewPropertyAnimator;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
-
+Handler handler;
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+        handler=new Handler();
+
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 Intent intent=new Intent(SplashActivity.this,RegisterActivity.class);
                 startActivity(intent);
-
                 finish();
             }
-        },2000);
-    }
-    }
+        },SPLASH_SCREEN_TIME_OUT);
 
+
+            /**
+             new Handler().postDelayed(new Runnable() {
+            @Override public void run() {
+            Intent intent=new Intent(SplashActivity.this,RegisterActivity.class);
+            startActivity(intent);
+
+            finish();
+            }
+            },2000);
+             **/
+        }
+}
